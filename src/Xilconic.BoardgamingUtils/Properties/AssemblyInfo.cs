79941aee1750c69cd221fbfccfd847b267b77266
Xilconic <Xilconic@users.noreply.github.com>
2017-01-27 21:33:45 +0100

    App icon and bare bones GUI

--- src/Xilconic.BoardgamingUtils/Properties/AssemblyInfo.cs
+++ src/Xilconic.BoardgamingUtils/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Xilconic.BoardgamingUtils")]
-[assembly: AssemblyDescription("Libary with boardgaming related classes.")]
+[assembly: AssemblyDescription("Library with boardgaming related classes.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Xilconic")]
 [assembly: AssemblyProduct("Xilconic.BoardgamingUtils")]
